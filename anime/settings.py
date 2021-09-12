b4744018830402697c06329f7470851b27edc2fb
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-13 00:31:53 +0300

    update settings for the database.

--- anime/settings.py
+++ anime/settings.py
@@ -13,7 +13,13 @@ env.read_env()
 
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
-SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
+uri = env.str("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
+else:
+    SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
+
 SECRET_KEY = env.str("SECRET_KEY")
 SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT")
 BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)
