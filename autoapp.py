c73ba6718b04d1b962224ea23893dfa9c5aa44ad
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-13 00:21:05 +0300

    update autoapp.py

--- autoapp.py
+++ autoapp.py
@@ -3,3 +3,10 @@
 from anime.app import create_app
 
 app = create_app()
+import os
+import re
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
